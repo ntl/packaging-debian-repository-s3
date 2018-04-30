dc5e9423fae3130158b35d110db2878d19bb6c27
Nathan Ladd <nathanladd+github@gmail.com>
2018-04-30 10:12:01 -0500

    Register package overwrites existing file information

---
+++ test/automated/commands/register_package/index_already_contains_package.rb
@@ -0,0 +1,38 @@
+require_relative '../../automated_init'
+
+context "Commands" do
+  context "Register Package" do
+    context "Index Already Contains Package" do
+      prior_index_entry = Controls::PackageIndex::Entry.example
+      index_entry = Controls::PackageIndex::Entry::Alternate.example(filename: prior_index_entry.filename)
+
+      assert(index_entry.filename == prior_index_entry.filename)
+      refute(index_entry == prior_index_entry)
+
+      package_index = Controls::PackageIndex.example(entries: [prior_index_entry])
+
+      distribution = Controls::Distribution.example
+      architecture = index_entry.architecture or fail
+
+      register_package = Commands::Package::Register.new(distribution)
+
+      package_index_store = register_package.package_index_store
+
+      package_index_store.add(package_index)
+
+      test "Does not raise error" do
+        refute proc { register_package.(index_entry) } do
+          raises_error?(Packaging::Debian::Schemas::Repository::PackageIndex::EntryAddedError)
+        end
+      end
+
+      test "Overwrites existing package information" do
+        overwritten = package_index_store.put? do |package_index|
+          package_index.entries == [index_entry]
+        end
+
+        assert(overwritten)
+      end
+    end
+  end
+end
