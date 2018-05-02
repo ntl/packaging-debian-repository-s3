16854fdb9d6b3982cbf96c35c44acca4c16076ee
Nathan Ladd <nathanladd+github@gmail.com>
2018-05-02 09:42:41 -0500

    Alternate signed release text control is implemented

--- lib/packaging/debian/repository/s3/controls/release.rb
+++ lib/packaging/debian/repository/s3/controls/release.rb
@@ -8,14 +8,26 @@ module Packaging
           module Release
             module Text
               module Signed
-                def self.example
-                  unsigned_text = Text.example
+                def self.example(unsigned_text=nil)
+                  unsigned_text ||= Text.example
 
                   GPG::Clearsign::Signature::Add.(unsigned_text)
                 end
 
                 def self.stream
                   StringIO.new(example)
+                end
+              end
+            end
+
+            module Alternate
+              module Text
+                module Signed
+                  def self.example
+                    unsigned_text = Text.example
+
+                    Release::Text::Signed.example(unsigned_text)
+                  end
                 end
               end
             end
